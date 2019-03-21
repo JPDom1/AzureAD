ab9e242f684a471d260d513070111b4cb8696aa2
JPDom1 <47320002+JPDom1@users.noreply.github.com>
2019-03-21 12:23:33 +0000

    Check MFA status of a user
    run this to check if a single user has MFA enabled and the current status of MFA for that user.

---
+++ Office 365 - Check MFA status of a user.ps1
@@ -0,0 +1,6 @@
+﻿#JP Erasmus 21/03/2019 :: Use at own risk.
+#Connect to Office 365
+Connect-MsolService
+#Input users UPN (user@domain.com) to show if MFA is enabled. Blank result = Disabled.
+$User = Get-MSolUser -UserPrincipalName user@domain.com
+$User.StrongAuthenticationMethods
